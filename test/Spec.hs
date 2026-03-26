bf57ce1ec8fe2b51b397116aafb3e62fdac9308a
David Himmelstrup <lemmih@gmail.com>
2026-03-26 11:14:05 +0100

    feat: add 10-minute timeout to QuickCheck properties and 20-minute timeout to CI (#347)

--- test/Spec.hs
+++ test/Spec.hs
@@ -25,6 +25,9 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Test.Tasty.QuickCheck as QC
 
+tenMinutes :: Timeout
+tenMinutes = Timeout (10 * 60 * 1000000) "10m"
+
 main :: IO ()
 main = buildTests >>= defaultMain
 
@@ -72,13 +75,14 @@ buildTests = do
             testCase "generated identifiers reject standalone underscore" test_generatedIdentifiersRejectStandaloneUnderscore,
             testCase "shrunk identifiers reject standalone underscore" test_shrunkIdentifiersRejectStandaloneUnderscore
           ],
-        testGroup
-          "properties"
-          [ QC.testProperty "generated expr AST pretty-printer round-trip" prop_exprPrettyRoundTrip,
-            QC.testProperty "generated module AST pretty-printer round-trip" prop_modulePrettyRoundTrip,
-            QC.testProperty "generated pattern AST pretty-printer round-trip" prop_patternPrettyRoundTrip,
-            QC.testProperty "generated type AST pretty-printer round-trip" prop_typePrettyRoundTrip
-          ],
+        adjustOption (const tenMinutes) $
+          testGroup
+            "properties"
+            [ QC.testProperty "generated expr AST pretty-printer round-trip" prop_exprPrettyRoundTrip,
+              QC.testProperty "generated module AST pretty-printer round-trip" prop_modulePrettyRoundTrip,
+              QC.testProperty "generated pattern AST pretty-printer round-trip" prop_patternPrettyRoundTrip,
+              QC.testProperty "generated type AST pretty-printer round-trip" prop_typePrettyRoundTrip
+            ],
         h2010,
         extensions,
         extensionMappingTests,
