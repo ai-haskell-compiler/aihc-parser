4e96c1a0d0d5577be45a8ee98b207e1252ba5e2b
David Himmelstrup <lemmih@gmail.com>
2026-04-06 20:06:54 +0200

    test(parser): cover standalone kind signature for type operator (#526)

---
+++ test/Test/Fixtures/oracle/StandaloneKindSignatures/standalone-kind-type-operator-data.hs
@@ -0,0 +1,10 @@
+{- ORACLE_TEST xfail parser support pending -}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+
+module StandaloneKindTypeOperatorData where
+
+import Data.Kind (Type)
+
+type (:+:) :: Type -> Type -> Type
+data a :+: b = L
