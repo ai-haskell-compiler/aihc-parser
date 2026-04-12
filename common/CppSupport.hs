670afecd8e243a97e84647d904b9274960c3584c
David Himmelstrup <lemmih@gmail.com>
2026-04-12 09:37:37 +0200

    refactor(cpp): hoist CPP macro machinery into aihc-hackage (#696)

--- common/CppSupport.hs
+++ common/CppSupport.hs
@@ -19,14 +19,13 @@ import Aihc.Cpp
     defaultConfig,
     preprocess,
   )
+import Aihc.Hackage.Cpp qualified as HackageCpp
 import Aihc.Parser.Lex (readModuleHeaderExtensions, readModuleHeaderPragmas)
 import Aihc.Parser.Syntax (Extension (CPP), ExtensionSetting (..), ModuleHeaderPragmas (..))
 import Data.ByteString (ByteString)
 import Data.Char (toLower)
 import Data.Functor.Identity (Identity (..), runIdentity)
-import Data.Map.Strict qualified as M
-import Data.Maybe (fromMaybe, mapMaybe)
-import Data.Set qualified as S
+import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
@@ -38,12 +37,12 @@ preprocessForParser inputFile deps resolveInclude source =
 
 preprocessForParserWithCppOptions :: (Monad m) => [String] -> FilePath -> [Text] -> (IncludeRequest -> m (Maybe ByteString)) -> Text -> m Result
 preprocessForParserWithCppOptions cppOptions inputFile deps resolveInclude source = do
-  let minVersionMacros = minVersionMacroNamesFromDeps deps
-  let injected = injectSyntheticCppMacros cppOptions minVersionMacros source
+  let minVersionMacros = HackageCpp.minVersionMacroNamesFromDeps deps
+  let injected = HackageCpp.injectSyntheticCppMacros cppOptions minVersionMacros source
   let cfg =
         defaultConfig
           { configInputFile = inputFile,
-            configMacros = cppMacrosFromOptions cppOptions
+            configMacros = HackageCpp.cppMacrosFromOptions cppOptions
           }
   drive (preprocess cfg (TE.encodeUtf8 injected))
   where
@@ -89,51 +88,6 @@ cppEnabledInSettings = foldl apply False
         DisableExtension CPP -> False
         _ -> enabled
 
-cppMacrosFromOptions :: [String] -> M.Map Text Text
-cppMacrosFromOptions cppOptions =
-  foldl apply builtinCppMacros (mapMaybe parseCppMacroOption cppOptions)
-  where
-    apply macros option =
-      case option of
-        CppDefine name value -> M.insert name value macros
-        CppUndef name -> M.delete name macros
-
-builtinCppMacros :: M.Map Text Text
-builtinCppMacros =
-  M.fromList
-    [ ("__GLASGOW_HASKELL__", "906"),
-      ("__GLASGOW_HASKELL_FULL_VERSION__", "\"9.6.7\""),
-      ("__GLASGOW_HASKELL_PATCHLEVEL1__", "7"),
-      ("__GLASGOW_HASKELL_PATCHLEVEL2__", "0")
-    ]
-
-data CppMacroOption
-  = CppDefine Text Text
-  | CppUndef Text
-
-parseCppMacroOption :: String -> Maybe CppMacroOption
-parseCppMacroOption raw =
-  let opt = T.strip (stripWrappingQuotes (T.pack raw))
-   in case T.stripPrefix "-D" opt of
-        Just rest ->
-          case T.breakOn "=" rest of
-            (name, "") | validMacroName name -> Just (CppDefine name "1")
-            (name, value) | validMacroName name -> Just (CppDefine name (T.drop 1 value))
-            _ -> Nothing
-        Nothing ->
-          case T.stripPrefix "-U" opt of
-            Just name | validMacroName name -> Just (CppUndef name)
-            _ -> Nothing
-
-validMacroName :: Text -> Bool
-validMacroName = not . T.null . T.strip
-
-stripWrappingQuotes :: Text -> Text
-stripWrappingQuotes txt =
-  if T.length txt >= 2 && T.head txt == '"' && T.last txt == '"'
-    then T.dropEnd 1 (T.drop 1 txt)
-    else txt
-
 normalizeSourceForParser :: FilePath -> Text -> Text
 normalizeSourceForParser inputFile =
   unliterateIfNeeded inputFile . stripLeadingBom
@@ -162,43 +116,3 @@ unliterateIfNeeded inputFile source
       | T.strip line == "\\end{code}" = "" : unlitLatex False rest
       | inCode = line : unlitLatex inCode rest
       | otherwise = "" : unlitLatex inCode rest
-
-minVersionMacroNamesFromDeps :: [Text] -> S.Set Text
-minVersionMacroNamesFromDeps = S.fromList . map toMinVersionMacroName
-  where
-    toMinVersionMacroName pkg = "MIN_VERSION_" <> T.map sanitizePkgChar pkg
-    sanitizePkgChar '-' = '_'
-    sanitizePkgChar c = c
-
-injectSyntheticCppMacros :: [String] -> S.Set Text -> Text -> Text
-injectSyntheticCppMacros cppOptions minVersionMacroNames source =
-  let existingFromOptions = cppDefinedOrUndefinedFromOptions cppOptions
-      reservedCompilerMinVersionNames = S.fromList ["MIN_VERSION_ghc", "MIN_VERSION_GLASGOW_HASKELL"]
-      shouldDefine name = not (name `S.member` existingFromOptions)
-      compilerMacroLines =
-        [ "#define MIN_VERSION_ghc(major1,major2,minor) 1"
-        | shouldDefine "MIN_VERSION_ghc"
-        ]
-          ++ [ "#define MIN_VERSION_GLASGOW_HASKELL(ma,mi,pl1,pl2) 1"
-             | shouldDefine "MIN_VERSION_GLASGOW_HASKELL"
-             ]
-      dynamicLines =
-        [ "#define " <> name <> "(major1,major2,minor) 1"
-        | name <- S.toAscList minVersionMacroNames,
-          not (name `S.member` reservedCompilerMinVersionNames),
-          shouldDefine name
-        ]
-      header =
-        if null (compilerMacroLines ++ dynamicLines)
-          then ""
-          else T.unlines (compilerMacroLines ++ dynamicLines)
-   in if T.null header then source else header <> source
-
-cppDefinedOrUndefinedFromOptions :: [String] -> S.Set Text
-cppDefinedOrUndefinedFromOptions =
-  foldl addName S.empty . mapMaybe parseCppMacroOption
-  where
-    addName acc option =
-      case option of
-        CppDefine name _ -> S.insert name acc
-        CppUndef name -> S.insert name acc
